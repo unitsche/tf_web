4affaf45849179e3fedf1e266c10134430ce1a3b
un <un@ulis-MacBook.fritz.box>
2016-12-04 18:23:03 +0100

    removing aws access keys to awscli instead of prompting...

--- variables.tf
+++ variables.tf
@@ -1,24 +1,22 @@
-variable "access_key" {
-  description = "The AWS access key."
-}
-variable "secret_key" {
-  description = "The AWS secret key."
-}
 variable "region" {
-   description = "The AWS region."
+  description = "The AWS region."
 }
+
 variable "key_name" {
   description = "The AWS key pair to use for resources."
 }
+
 variable "ami" {
-  type    = "map"
+  type        = "map"
   description = "A map of AMIs."
-  default = {}
+  default     = {}
 }
+
 variable "instance_type" {
   description = "The instance type."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
+
 variable "instance_ips" {
   description = "The IPs to use for our instances"
   default     = ["10.0.1.20", "10.0.1.21"]
